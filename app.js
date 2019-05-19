296ad500ffc7a4955a09e7b21ec204543c0ec697
Senenko Vitaliy <senenkovitalik@gmail.com>
2019-05-19 22:02:03 +0300

    feat: GraphQL + MongoDB

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const graphqlHttp = require('express-graphql');
-const {buildSchema} = require('graphql');
+const { buildSchema } = require('graphql');
+const mongoose = require('mongoose');
+const Event = require('./models/event');
 
 const app = express();
-
-const events = [];
 
 app.use(bodyParser.json());
 
 app.use('/graphql',
-    graphqlHttp({
-        schema: buildSchema(`
+  graphqlHttp({
+    schema: buildSchema(`
             type Event {
                 _id: ID!
                 title: String!
@@ -40,23 +40,43 @@ app.use('/graphql',
                 mutation: RootMutation
             }
         `),
-        rootValue: {
-            events: () => {
-                return events;
-            },
-            createEvent: args => {
-                const event = {
-                    _id: Math.random().toString(),
-                    title: args.eventInput.title,
-                    description: args.eventInput.description,
-                    price: +args.eventInput.price,
-                    date: args.eventInput.date
-                };
-                events.push(event);
-                return event;
-            }
-        },
-        graphiql: true
-    }));
+    rootValue: {
+      events: () => {
+        return Event.find().then(events => {
+          return events.map(event => {
+            return { ...event._doc }
+          });
+        }).catch(err => {
+          console.log(err);
+          throw err;
+        });
+      },
+      createEvent: args => {
+        const event = new Event({
+          title: args.eventInput.title,
+          description: args.eventInput.description,
+          price: +args.eventInput.price,
+          date: new Date(args.eventInput.date)
+        });
+        return event
+          .save()
+          .then(event => {
+            return event;
+          })
+          .catch(err => {
+            console.log(err);
+            throw err;
+          });
+      }
+    },
+    graphiql: true
+  }));
 
-app.listen(3000);
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-cviiz.mongodb.net/${process.env.MONGO_DB}?retryWrites=true`, { useNewUrlParser: true })
+  .then(() => {
+    app.listen(3000, () => {
+      console.log('Start server at http://localhost:3000');
+    });
+  }).catch(err => {
+  console.log(err);
+});
