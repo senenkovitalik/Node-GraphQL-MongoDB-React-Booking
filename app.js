3a0a44c20b1aa1ea9605e66ef91ef2d12e5bff1f
Senenko Vitaliy <senenkovitalik@gmail.com>
2019-05-20 23:05:12 +0300

    feat: dynamic relations

--- app.js
+++ app.js
@@ -1,133 +1,27 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const graphqlHttp = require('express-graphql');
-const { buildSchema } = require('graphql');
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
 
-const Event = require('./models/event');
-const User = require('./models/user');
+const schema = require('./graphql/schema/index');
+const resolvers = require('./graphql/resolvers/index');
 
 const app = express();
 
 app.use(bodyParser.json());
 
 app.use('/graphql',
-  graphqlHttp({
-    schema: buildSchema(`
-            type Event {
-                _id: ID!
-                title: String!
-                description: String!
-                price: Float!
-                date: String!
-            }
-            
-            input EventInput {
-                title: String!
-                description: String!
-                price: Float!
-                date: String!
-            }
-            
-            type User {
-                _id: ID!,
-                email: String!
-                password: String
-            }
-            
-            input UserInput {
-                email: String!
-                password: String!
-            }
-            
-            type RootQuery {
-                events: [Event!]!
-            }
-            
-            type RootMutation {
-                createEvent(eventInput: EventInput): Event
-                createUser(userInput: UserInput): User
-            }
-             
-            schema {
-                query: RootQuery
-                mutation: RootMutation
-            }
-        `),
-    rootValue: {
-      events: () => {
-        return Event.find().then(events => {
-          return events.map(event => {
-            return { ...event._doc }
-          });
-        }).catch(err => {
-          console.log(err);
-          throw err;
-        });
-      },
-      createEvent: args => {
-        const event = new Event({
-          title: args.eventInput.title,
-          description: args.eventInput.description,
-          price: +args.eventInput.price,
-          date: new Date(args.eventInput.date),
-          creator: '5ce1b1053dace527ea6d896e'
-        });
-        let createdEvent;
-        return event
-          .save()
-          .then(event => {
-            createdEvent = event;
-            return User.findById('5ce1b1053dace527ea6d896e');
-          })
-          .then(user => {
-            if (!user) {
-              throw new Error('User already exists.');
-            }
-            user.createdEvents.push(event);
-            return user.save();
-          })
-          .then(() => {
-            return createdEvent;
-          })
-          .catch(err => {
-            console.log(err);
-            throw err;
-          });
-      },
-      createUser: args => {
-        return User.findOne({ email: args.userInput.email })
-          .then(user => {
-            if (user) {
-              throw new Error('User doesn\'t exists.');
-            }
-            return bcrypt.hash(args.userInput.password, 12);
-          })
-          .then(hashedPassword => {
-            const user = new User({
-              email: args.userInput.email,
-              password: hashedPassword
-            });
-            return user.save();
-          })
-          .then(user => {
-            const { password, ...rest } = user._doc;
-            return { ...rest, password: null };
-          })
-          .catch(err => {
-            throw err;
-          });
-      }
-    },
-    graphiql: true
-  }));
+    graphqlHttp({
+        schema,
+        rootValue: resolvers,
+        graphiql: true
+    }));
 
-mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-cviiz.mongodb.net/${process.env.MONGO_DB}?retryWrites=true`, { useNewUrlParser: true })
-  .then(() => {
-    app.listen(3000, () => {
-      console.log('Start server at http://localhost:3000');
-    });
-  }).catch(err => {
-  console.log(err);
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-cviiz.mongodb.net/${process.env.MONGO_DB}?retryWrites=true`, {useNewUrlParser: true})
+    .then(() => {
+        app.listen(3000, () => {
+            console.log('Start server at http://localhost:3000');
+        });
+    }).catch(err => {
+    console.log(err);
 });
