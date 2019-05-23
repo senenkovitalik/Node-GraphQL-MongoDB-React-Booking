ebd1bedb699195fbc075cc264fa8cae6a1893ded
Senenko Vitaliy <senenkovitalik@gmail.com>
2019-05-23 21:42:47 +0300

    feat: react setup

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+
+import AuthPage from './pages/Auth';
+import EventsPage from './pages/Events';
+import BookingsPage from './pages/Bookings';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Redirect from="/" to="/auth" exact />
+        <Route path="/auth" component={AuthPage}/>
+        <Route path="/events" component={EventsPage}/>
+        <Route path="/bookings" component={BookingsPage}/>
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default App;
