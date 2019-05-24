b2396a1c0bb8a497c475c0fe9a6954e0066c1519
Senenko Vitaliy <senenkovitalik@gmail.com>
2019-05-24 17:21:29 +0300

    feat: Navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,16 +4,24 @@ import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import AuthPage from './pages/Auth';
 import EventsPage from './pages/Events';
 import BookingsPage from './pages/Bookings';
+import MainNavigation from '../src/components/Navigation/MainNavigation';
+
+import '../src/App.css';
 
 function App() {
   return (
     <BrowserRouter>
-      <Switch>
-        <Redirect from="/" to="/auth" exact />
-        <Route path="/auth" component={AuthPage}/>
-        <Route path="/events" component={EventsPage}/>
-        <Route path="/bookings" component={BookingsPage}/>
-      </Switch>
+      <React.Fragment>
+        <MainNavigation/>
+        <main className="main-content">
+          <Switch>
+            <Redirect from="/" to="/auth" exact/>
+            <Route path="/auth" component={AuthPage}/>
+            <Route path="/events" component={EventsPage}/>
+            <Route path="/bookings" component={BookingsPage}/>
+          </Switch>
+        </main>
+      </React.Fragment>
     </BrowserRouter>
   );
 }
