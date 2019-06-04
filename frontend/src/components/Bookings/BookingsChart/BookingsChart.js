725ab21f8c76785cf55be8540311e6af407ddaf9
Senenko Vitaliy <senenkovitalik@gmail.com>
2019-06-04 14:15:18 +0300

    feat: chart

--- frontend/src/components/Bookings/BookingsChart/BookingsChart.js
+++ frontend/src/components/Bookings/BookingsChart/BookingsChart.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import { Bar as BarChart } from 'react-chartjs';
+
+import './BookingsChart.css';
+
 
 const BOOKING_BUCKETS = {
   'Cheap': {
@@ -15,20 +19,46 @@ const BOOKING_BUCKETS = {
   }
 };
 
-const bookingsChart = ({bookings}) => {
-  const output = [];
+const bookingsChart = ({ bookings }) => {
+  const chartData = {
+    labels: [],
+    datasets: [{
+      data: [],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)'
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)'
+      ],
+      borderWidth: 1
+    }]
+  };
+
   for (const bucket in BOOKING_BUCKETS) {
-    const filteredBookingsCount = bookings.reduce((prev, current) =>
-      current.event.price > BOOKING_BUCKETS[bucket].min &&
-      current.event.price < BOOKING_BUCKETS[bucket].max
-        ? prev + 1
-        : prev
-      , 0);
-    output[bucket] = filteredBookingsCount;
+    const filteredBookingCount = bookings.reduce((prev, current) =>
+        current.event.price > BOOKING_BUCKETS[bucket].min &&
+        current.event.price < BOOKING_BUCKETS[bucket].max
+          ? prev + 1
+          : prev,
+      0);
+    chartData.labels.push(bucket);
+    chartData.datasets[0].data.push(filteredBookingCount);
   }
-  console.log(output);
+
   return (
-    <h1>Chart Component</h1>
+    <div className='booking-chart'>
+      <BarChart data={chartData}/>
+    </div>
   );
 };
 
